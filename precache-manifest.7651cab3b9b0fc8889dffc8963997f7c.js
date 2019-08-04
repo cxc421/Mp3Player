6e5a67727e0c1532dd3b15f1853be42a7412d268
Chris Chuang <chrischuang0421@gmail.com>
2019-08-04 21:33:45 +0800

    Updates

---
+++ precache-manifest.7651cab3b9b0fc8889dffc8963997f7c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc46f765f4af1e3499a6db753535a946",
+    "url": "/Mp3Player/index.html"
+  },
+  {
+    "revision": "3b620ddf5ff16795fa65",
+    "url": "/Mp3Player/static/css/main.d31f2474.chunk.css"
+  },
+  {
+    "revision": "742a73c21a63bbde2c4e",
+    "url": "/Mp3Player/static/js/2.170e971d.chunk.js"
+  },
+  {
+    "revision": "3b620ddf5ff16795fa65",
+    "url": "/Mp3Player/static/js/main.619382f4.chunk.js"
+  },
+  {
+    "revision": "19767b5cf0b33b8479b6",
+    "url": "/Mp3Player/static/js/runtime~main.ce50c9f3.js"
+  },
+  {
+    "revision": "abc0b1c0d1bff2871f0f010588c0f797",
+    "url": "/Mp3Player/static/media/Embody - Dreamin (ft. Camden Cox).abc0b1c0.mp3"
+  },
+  {
+    "revision": "75035dee817fca4779ffb6a290d344cd",
+    "url": "/Mp3Player/static/media/Embody - I Miss You.75035dee.mp3"
+  },
+  {
+    "revision": "a61100edb8f08e241905aeb62dbf05ac",
+    "url": "/Mp3Player/static/media/Embody - Without You (feat. Jack Hawitt).a61100ed.mp3"
+  },
+  {
+    "revision": "956766f6ae4956f7e7e18c0e2bffa35f",
+    "url": "/Mp3Player/static/media/Jonas Brothers - Happiness Begins.956766f6.mp3"
+  },
+  {
+    "revision": "094bc70cf50236e4221fea483749e917",
+    "url": "/Mp3Player/static/media/Jonas_A0_Rectangle_52_pattern@2x_cut.094bc70c.png"
+  },
+  {
+    "revision": "e04a1703897f2f67cea4cc6066c71cff",
+    "url": "/Mp3Player/static/media/SF-Pro-Text-Semibold.e04a1703.ttf"
+  },
+  {
+    "revision": "468fad7e5e65be8be4c18d474a72308f",
+    "url": "/Mp3Player/static/media/[Clean] Joel Adams - Coffee.468fad7e.mp3"
+  },
+  {
+    "revision": "c9a08561480335f387d940b842ff765c",
+    "url": "/Mp3Player/static/media/ad.c9a08561.png"
+  },
+  {
+    "revision": "aedcb32f86b63af7f1598784bae03d6a",
+    "url": "/Mp3Player/static/media/coffee_A0_Path_4_pattern@2x_cut2.aedcb32f.png"
+  },
+  {
+    "revision": "0298b56320f53db772834adf29911632",
+    "url": "/Mp3Player/static/media/embody_A0_Rectangle_50_pattern@2x.0298b563.png"
+  },
+  {
+    "revision": "18a702e632c82ff9c2e9ccbcee08b0f3",
+    "url": "/Mp3Player/static/media/song-a.18a702e6.jpg"
+  },
+  {
+    "revision": "104df9cf730e0edd3a408df47f0945d1",
+    "url": "/Mp3Player/static/media/song-b.104df9cf.jpg"
+  },
+  {
+    "revision": "c04c04822278174ed307223f20f29cdd",
+    "url": "/Mp3Player/static/media/song-c.c04c0482.jpg"
+  }
+]);
